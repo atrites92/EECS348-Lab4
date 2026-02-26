c6c0b1a089ff2c8f9a68e9b39d76cb69c5285394
Aaron <atrites92@ku.edu>
2026-02-26 13:46:35 -0600

    added user choices and prompt

--- tempConversions.c
+++ tempConversions.c
@@ -1,6 +1,22 @@
 // @author Aaron Trites
 #include<stdio.h>
 int main(){
+    //Variable declaration
+    float user_temperature;
+    int user_type;
+    int user_conversion;
+
+    //Get user input
+    printf("Enter temperature: ");
+    scanf("%d", user_temperature);
+    printf("\nTemperature types: ");
+    printf("\nFahrnheit (1)");
+    printf("\nCelsius (2)");
+    printf("\nKelvin (3)");
+    printf("\nEnter type (1, 2, 3): ");
+    scanf("%d", user_type);
+    printf("\nEner type to conver to (1, 2, 3): ");
+    scanf("%d", user_conversion);
 
   return 0;
 }
